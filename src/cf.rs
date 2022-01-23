218a30e31b91b936fd3be87738699768fda71c8b
Daniel <contact@jiftoo.dev>
2022-01-23 22:14:06 +0300

    continuous_fraction as iter

--- src/cf.rs
+++ src/cf.rs
@@ -1,21 +1,22 @@
 pub fn dec_to_fraction(n: f64) -> (u64, u64) {
-    find_convergents(&continued_fraction(n))
+    find_convergents(&continued_fraction(n).take(16).collect::<Vec<u64>>())
 }
 
-fn continued_fraction(mut n: f64) -> Vec<u64> {
-    const MAX_STEPS: u32 = 16;
-    let mut coef: Vec<u64> = Vec::new();
-    for _ in 0..MAX_STEPS {
-        let i = n.floor() as u64;
-        coef.push(i);
-        let f = n - i as f64;
+fn continued_fraction(mut n: f64) -> impl Iterator<Item = u64> {
+    let mut done = false;
+    std::iter::from_fn(move || {
+        if done {
+            return None;
+        }
+        let i = n.floor();
+        let f = n - i;
         if f <= 1e-6 {
-            break;
+            done = true;
         } else {
             n = 1.0 / f;
         }
-    }
-    return coef;
+        Some(i as u64)
+    })
 }
 
 fn find_convergents(vec: &[u64]) -> (u64, u64) {
